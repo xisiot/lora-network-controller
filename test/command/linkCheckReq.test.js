1f8dc4c9a606c7454c1ee69b84435169ed47a323
zqh <530068718@qq.com>
2018-08-03 16:33:11 +0800

    first version

---
+++ test/command/linkCheckReq.test.js
@@ -0,0 +1,13 @@
+const expect = require('chai').expect;
+
+const devAddr = '0x00046354';
+const cid = 0x02;
+
+describe('Test LinkCheckReq', function () {
+  it('send uplink LinkCheckReq', function () {
+
+    // TODO
+    expect(cid).to.be.equal(0x02);
+  });
+
+});
